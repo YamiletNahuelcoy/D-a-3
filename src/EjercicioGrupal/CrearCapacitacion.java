617cc5614f905130cc6e409a209b9dd3e72ece7f
YamiletNahuelcoy <74574893+YamiletNahuelcoy@users.noreply.github.com>
2020-12-29 12:44:57 -0300

    Eliminación body contacto
    Error al pegar body en página contacto

--- src/EjercicioGrupal/CrearCapacitacion.java
+++ src/EjercicioGrupal/CrearCapacitacion.java
@@ -27,7 +27,8 @@ public class CrearCapacitacion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("CrearCapacitacion.jsp").forward(request, response);
 	}
 
 	/**
