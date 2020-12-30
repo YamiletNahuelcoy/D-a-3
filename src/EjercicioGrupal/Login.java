14071e15c4ca9a6458d36b1d1d908b1d8604e3bf
YamiletNahuelcoy <74574893+YamiletNahuelcoy@users.noreply.github.com>
2020-12-30 13:04:10 -0300

    Desarrollo Día 4
    Versión 1

--- src/EjercicioGrupal/Login.java
+++ src/EjercicioGrupal/Login.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Contacto
+ * Servlet implementation class Login
  */
-@WebServlet("/Contacto")
-public class Contacto extends HttpServlet {
+@WebServlet("/Login")
+public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Contacto() {
+    public Login() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,9 +28,7 @@ public class Contacto extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-
-		request.getRequestDispatcher("Contacto.jsp").forward(request, response);
-
+		request.getRequestDispatcher("Login.jsp").forward(request, response);
 	}
 
 	/**
